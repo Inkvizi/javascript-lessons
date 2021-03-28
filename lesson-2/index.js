d4466efdc5791ec29e2f883fc45603448951ac9b
marinets <Marinets.Ev@sberbank.ru>
2021-03-28 18:39:37 +0300

    Домашнее задание 4

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,66 +1,15 @@
-const media = [{
-    name: 'Шерлок',
-    type: 'tvshow',
-    year: '2016',
-    episodes: [{
-        name: 'Знакомство с Шерлоком',
-        episodeNumber: '10'
-    }, {
-        name: 'Собака Баскервили',
-        episodeNumber: '1'
-    }]
-}, {
-    name: 'Шрек',
-    year: '2014',
-    type: 'movie'
-}, {
-    name: 'Декстер',
-    type: 'tvshow',
-    year: '2010',
-    episodes: [{
-        name: 'Побег',
-        episodeNumber: '2'
-    }, {
-        name: 'Жизнь после',
-        episodeNumber: '3'
-    }, {
-        name: 'Первая история декстера',
-        episodeNumber: '1'
-    }]
-}, 
-];
-
-const filteredMedia = media.filter(item => item.type === 'tvshow');
-filteredMedia.sort(function (tvshowLeft, tvshowRight) {
-    if (tvshowLeft.year > tvshowRight.year) {
-      return 1;
-    }
-    if (tvshowLeft.year < tvshowRight.year) {
-      return -1;
-    }
-    return 0;
-  });
-let episodesList = [];
-filteredMedia.forEach(media => {
-    sortedEpisodes = media.episodes.sort(function (episodeLeft, episodeRight) {
-        if (episodeLeft.episodeNumber > episodeRight.episodeNumber) {
-          return 1;
-        }
-        if (episodeLeft.episodeNumber < episodeRight.episodeNumber) {
-          return -1;
-        }
-        return 0;
-      });
-      episodesList = episodesList.concat(sortedEpisodes.map(episode => {
-        return episode.name;                                   
-    }))
-})
-
-console.log(episodesList);
-  
-
-// список всех эпизодов по порядку
-// 1 - получить все tv show
-// 2 - episodes // sort
-// 3 - ...
-['Знакомство с Шерлоком', 'Собака Баскервили', 'Первая история декстера', 'Побег', 'Жизнь после']
+//Домашнее задание: Задача: антипаттерн - напиши данный код максимально коротко! Победит тот, чей код будет короче
+/*
+let y;
+const x = '' + prompt('Введите число', -1);
+if (x < 0 && x > -2) {
+ y = 0;
+} else {
+ y = 2;
+}
+if (y === 0) {
+    alert('Делить на ноль нельзя')
+}
+*/
+//Мое решение:
+~x?2:alert('Делить на ноль нельзя');
