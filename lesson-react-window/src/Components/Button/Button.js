90955a62a49d30fa2f3bc26a560c2cc4b355af1c
Маринец Евгений <16816725@sigma.sbrf.ru>
2021-05-04 16:40:37 +0300

    Домашнее задание с бегающей кнопкой

--- lesson-react-window/src/Components/Button/Button.js
+++ lesson-react-window/src/Components/Button/Button.js
@@ -1,101 +1,15 @@
-import { Component } from 'react';
-import React from 'react';
 import styles from './Button.module.css';
 
-const minDistanceForRun = 30;
-
-export class Button extends Component {
-    constructor (props) {
-        super(props);
-        this.parent = {
-            parentX: 0,
-            parentY: 0,
-            parentWidth: 0,
-            parentHeight: 0
-        }
-        this.myRef = React.createRef();
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        this.parent = {
-            parentX: nextProps.parentX,
-            parentY: nextProps.parentY,
-            parentWidth: nextProps.parentWidth,
-            parentHeight: nextProps.parentHeight
-        };
-        return this.handleMouseMove(nextProps.x, nextProps.y);
-    }
-
-    handleMouseMove(x, y) {
-        if (this.props.isTrue === "true") {
-            let button = this.myRef.current;
-            return false;
-        }
-        
-        if (this.checkRunCondition(x, y)) {  
-            let newPosition = this.calculateNewCoordinates(x, y);
-            let button = this.myRef.current;
-            button.style.offsetLeft = newPosition.newX;
-            button.style.offsetTop = newPosition.newY;
-            //console.log(button.style);
-            return true;
-        } 
-        return false;
-    }
-
-    checkRunCondition (x, y) {
-        if ((Math.abs(x - this.myRef.current.offsetLeft) < minDistanceForRun) && 
-            (Math.abs(y - this.myRef.current.offsetTop) < minDistanceForRun)
-        ) {
-            return true;
-        }
-        return false;
-    }
-
-    calculateNewCoordinates(x, y) {
-        let differenceDistanceX = this.myRef.current.offsetLeft - x;
-        let newX = differenceDistanceX > 0 ? 
-          this.myRef.current.offsetLeft + minDistanceForRun + differenceDistanceX : 
-          this.myRef.current.offsetLeft - minDistanceForRun + differenceDistanceX;
-        if (newX < this.parent.parentX) {
-            newX = this.parent.parentX + this.parent.parentWidth - this.myRef.current.clientWidth;
-        } else if (newX >  this.parent.parentX + this.parent.parentWidth - this.myRef.current.clientWidth) {
-            newX = this.parent.parentX;
-        }
-        let differenceDistanceY = this.myRef.current.offsetTop - y;
-        let newY = differenceDistanceY > 0 ? 
-          this.myRef.current.offsetTop + minDistanceForRun + differenceDistanceY : 
-          this.myRef.current.offsetTop - minDistanceForRun + differenceDistanceY;
-        if (newY < this.parent.parentY) {
-            newY = this.parent.parentY + this.parent.parentTop - this.myRef.current.clientHeight;
-        } else if (newY >  this.parent.parentY + this.parent.parentTop - this.myRef.current.clientHeight) {
-            newY = this.parent.parentY;
-        }
-        return {newX, newY};
-    }
-    
-    getCaption (isTrue) {
-        if (isTrue === "true") {
-            return "Да, конечно!";
-        }
-        else {
-            return "Не очень";
-        }
-    }
-
-    render () {
-        // if (this.myRef!==null&&this.myRef.current!==null) {
-        //     return (
-        //         <div className={styles._} ref={this.myRef} style={this.myRef.current.style}>
-        //             <button className={styles.button} type="button">{this.getCaption(this.props.isTrue)}</button>
-        //         </div>
-        //     );    
-        // }
-        return (
-            <div className={styles._} ref={this.myRef}>
-                <button className={styles.button} type="button">{this.getCaption(this.props.isTrue)}</button>
-            </div>
-        );
-    }
+export function Button (props) {
+    return (
+        <div className={styles._}>
+            <button 
+                className={styles.button} 
+                type="button" 
+                onClick={props.onClick}
+            >
+                {props.caption}
+            </button>
+        </div>
+    );
 }
--- lesson-react-window/src/Components/Window/Window.js
+++ lesson-react-window/src/Components/Window/Window.js
@@ -1,59 +1,49 @@
 import styles from './Window.module.css';
-import {Button} from '../Button/Button';
-import { Component } from 'react';
-import React from 'react';
+import anime from "animejs";
+import {Button} from "../Button/Button";
+import {useRef, useEffect, useState} from 'react';
 
-export class Window extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mouseX: 0,
-            mouseY: 0,
-            x: 0,
-            y: 0,
-            width: 0,
-            height: 0
+export function Window() {
+    const animationRef = useRef(null);
+    const buttonRef = useRef(null);
+    const windowRef = useRef(null);
+    const [buttonLeft, setButtonLeft] = useState(0);
+    const [buttonTop, setButtonTop] = useState(0);
+    useEffect(() => {
+        if (buttonLeft === 0) {
+            setButtonLeft(buttonRef.current.offsetLeft);
         }
-        this.myRef = React.createRef();    
-    }
-
-    handleMouseMove(event) {
-        event.preventDefault();
-        this.setState({
-            mouseX: event.clientX,
-            mouseY: event.clientY,
-            x: this.myRef.current.offsetLeft,
-            y: this.myRef.current.offsetTop,
-            width: this.myRef.current.clientWidth,
-            height: this.myRef.current.clientHeight
-        })
-    }
-
-    render() {
-        return (
-            <div onMouseMove={(event) => this.handleMouseMove(event)} className={styles._} ref={this.myRef}>
-                <div className={styles.label}>Вам нравится курс по JavaScript?</div>
-                <div className={styles.bottomGroup}>
-                    <Button 
-                        isTrue="true" 
-                        x={this.state.mouseX} 
-                        y={this.state.mouseY} 
-                        parentX={this.state.x}
-                        parentY={this.state.y}
-                        parentWidth={this.state.width}
-                        parentHeigth={this.state.height}
-                    ></Button>
-                    <Button
-                        isTrue="" 
-                        x={this.state.mouseX} 
-                        y={this.state.mouseY} 
-                        parentX={this.state.x}
-                        parentY={this.state.y}
-                        parentWidth={this.state.width}
-                        parentHeigth={this.state.height} 
-                    ></Button>
-                </div>
+        if (buttonTop === 0) {
+            setButtonTop(buttonRef.current.offsetTop);
+        }
+        animationRef.current = anime({
+            targets: buttonRef.current,
+            left: buttonLeft,
+            top: buttonTop,
+            easing: "easeOutCirc"
+        });
+    }, [buttonLeft, buttonTop]); 
+    return (
+        <div className={styles._} ref={windowRef}>
+            <div className={styles.label}>Вам нравится курс по JavaScript?</div>
+            <div className={styles.button}>
+                <Button onClick={()=>{alert("Отлично!")}} caption="Да, конечно!"></Button>
             </div>
-        ) 
-    }
-} 
+            <div 
+                className={styles.jokeButton}
+                onMouseOver={()=>{
+                    setButtonLeft(getRandomDistance(windowRef.current.clientWidth - buttonRef.current.clientWidth));
+                    setButtonTop(getRandomDistance(windowRef.current.clientHeight - buttonRef.current.clientHeight));
+                }}
+                ref={buttonRef}
+            >
+                <Button onClick={()=>{alert("Вы очень настойчивы!")}} caption="Не знаю"></Button>    
+            </div> 
+            
+        </div>
+    );
+}
+
+const getRandomDistance = (maxDistance) => {
+    return Math.floor(Math.random() * (maxDistance));
+};
